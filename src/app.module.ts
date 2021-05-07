ff2bf1c525e09a6ad9c0dd550b853fbb366b59bb
cloudydong <cloudy12028@gmail.com>
2021-05-07 17:39:59 +0900

    create podcast.resolver & add GraphQLModule.forRoot in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PodcastModule } from './podcast/podcast.module';
 import { CommonModule } from './common/common.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
@@ -11,6 +12,9 @@ import { CommonModule } from './common/common.module';
       synchronize: true,
       logging: false,
       entities: ['dist/**/*.entity.{ts,js}'],
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
     }),
     CommonModule,
     PodcastModule,
--- src/podcast/podcast.resolver.ts
+++ src/podcast/podcast.resolver.ts
@@ -1,4 +1,104 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateEpisodeInput,
+  CreateEpisodeOutput,
+} from './dto/create-episode.dto';
+import {
+  CreatePodcastInput,
+  CreatePodcastOutput,
+} from './dto/create-podcast.dto';
+import {
+  DeleteEpisodeInput,
+  DeleteEpisodeOutput,
+} from './dto/delete-episode.dto';
+import {
+  DeletePodcastInput,
+  DeletePodcastOutput,
+} from './dto/delete-podcast.dto';
+import { FindEpisodeInput, FindEpisodeOutput } from './dto/find-episode.dto';
+import { FindEpisodesInput, FindEpisodesOutput } from './dto/find-episodes.dto';
+import { FindPodcastInput, FindPodcastOutput } from './dto/find-podcast.dto';
+import { FindPodcastsOutput } from './dto/find-podcasts.dto';
+import {
+  UpdateEpisodeInput,
+  UpdateEpisodeOutput,
+} from './dto/update-episode.dto';
+import {
+  UpdatePodcastInput,
+  UpdatePodcastOutput,
+} from './dto/update-podcast.dto';
+import { Podcast } from './entity/podcast.entity';
+import { PodcastService } from './podcast.service';
 
-@Resolver()
-export class PodcastResolver {}
+@Resolver(() => Podcast)
+export class PodcastResolver {
+  constructor(private readonly podcastService: PodcastService) {}
+
+  @Query(() => FindPodcastsOutput)
+  async findPodcasts(): Promise<FindPodcastsOutput> {
+    return this.podcastService.findPodcasts();
+  }
+
+  @Query(() => FindPodcastOutput)
+  async findPodcast(
+    @Args('input') findPodcastInput: FindPodcastInput,
+  ): Promise<FindPodcastOutput> {
+    return this.podcastService.findPodcast(findPodcastInput);
+  }
+
+  @Mutation(() => CreatePodcastOutput)
+  async createPodcast(
+    @Args('input') createPodcastInput: CreatePodcastInput,
+  ): Promise<CreatePodcastOutput> {
+    return this.podcastService.createPodcast(createPodcastInput);
+  }
+
+  @Mutation(() => UpdatePodcastOutput)
+  async updatePodcast(
+    @Args('input') updatePodcastInput: UpdatePodcastInput,
+  ): Promise<UpdatePodcastOutput> {
+    return this.podcastService.updatePodcast(updatePodcastInput);
+  }
+
+  @Mutation(() => DeletePodcastOutput)
+  async deletePodcast(
+    @Args('input') deletePodcastInput: DeletePodcastInput,
+  ): Promise<DeletePodcastOutput> {
+    return this.podcastService.updatePodcast(deletePodcastInput);
+  }
+
+  @Query(() => FindEpisodesOutput)
+  async findEpisodes(
+    @Args('input') findEpisodesInput: FindEpisodesInput,
+  ): Promise<FindEpisodesOutput> {
+    return this.podcastService.findEpisodes(findEpisodesInput);
+  }
+
+  @Query(() => FindEpisodeOutput)
+  async findEpisode(
+    @Args('input') findEpisodeInput: FindEpisodeInput,
+  ): Promise<FindEpisodeOutput> {
+    return this.podcastService.findPodcast(findEpisodeInput);
+  }
+
+  @Mutation(() => CreateEpisodeOutput)
+  async createEpisode(
+    @Args('input') createEpisodeInput: CreateEpisodeInput,
+  ): Promise<CreateEpisodeOutput> {
+    return this.podcastService.createEpisode(createEpisodeInput);
+  }
+
+  @Mutation(() => UpdateEpisodeOutput)
+  async updateEpisode(
+    @Args('input') updateEpisodeInput: UpdateEpisodeInput,
+  ): Promise<UpdateEpisodeOutput> {
+    return this.podcastService.updateEpisode(updateEpisodeInput);
+  }
+
+  @Mutation(() => DeleteEpisodeOutput)
+  async deleteEpisode(
+    @Args('input') deleteEpisodeInput: DeleteEpisodeInput,
+  ): Promise<DeleteEpisodeOutput> {
+    return this.podcastService.deleteEpisode(deleteEpisodeInput);
+  }
+}
