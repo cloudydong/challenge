5750b365436b4f14c52fbeae25512c50581dc327
cloudydong <cloudy12028@gmail.com>
2021-05-21 14:36:07 +0900

    DATABASE_URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,12 +20,15 @@ import { UserModule } from './user/user.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      ssl: true,
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+          }),
       synchronize: false,
       logging: false,
       entities: [Podcast, Episode, User, Review],
